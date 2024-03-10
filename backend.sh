0bfdc3802b0ff39cc0e967a1675da442cdebcf61
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-03-10 15:59:56 +0530

    Giving check status for every command whether it is success or failure

--- backend.sh
+++ backend.sh
@@ -11,57 +11,57 @@ fi
 
 Print_Tak_Heading "Disable default NodeJS Version Module"
 dnf module disable nodejs -y &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Enable NodeJS module for V20"
 dnf module enable nodejs:20 -y &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "install NodeJS"
 dnf install nodejs -y &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Adding Application user"
 useradd expense &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Copy Backend Service file"
 cp backend.service /etc/systemd/system/backend.service &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Clean the Old Content"
 rm -rf /app &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Creating app Directory"
 mkdir /app &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Download app Content"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Extract app Content"
 cd /app &>>/tmp/expense.log
 unzip /tmp/backend.zip &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 
 Print_Tak_Heading "Download NodeJS dependencies"
 cd /app &>>/tmp/expense.log
 npm install &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Start backend service"
 systemctl daemon-reload &>>/tmp/expense.log
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "Install MySql Client"
 dnf install mysql -y &>>/tmp/expense.log
-echo $?
+Check_Status $?
 
 Print_Tak_Heading "load Schema"
 mysql -h 172.31.8.154 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>/tmp/expense.log
-echo $?
+Check_Status $?
--- common.sh
+++ common.sh
@@ -2,3 +2,11 @@ Print_Tak_Heading() {
 echo $1
 echo "############# $1 ###############" &>>/tmp/expense.log
 }
+
+Check_Status() {
+  if [ $1 -eq 0 ]: then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
+}
