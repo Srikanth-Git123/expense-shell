5fb6b52fa1135a4c35e010cafccc4b5aecb57526
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-03-12 21:30:20 +0530

    Ip addresses

--- backend.sh
+++ backend.sh
@@ -52,5 +52,5 @@ dnf install mysql -y &>>$LOG
 Check_Status $?
 
 Print_Task_Heading "load Schema"
-mysql -h mysql-dev.psrikanth.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
+mysql -h 172.31.13.248 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOG
 Check_Status $?
